9b81b69667d90b3a6418bc4f87731c547ac1e309
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-03-24 01:31:02 +0000

    Game State Replication

--- Source/MastersProject/TankGameState.h
+++ Source/MastersProject/TankGameState.h
@@ -18,13 +18,13 @@ class MASTERSPROJECT_API ATankGameState : public AGameState
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Replicated)
 	int BlueTickets = 0.f;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Replicated)
 	int RedTickets = 0.f;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Replicated)
 	int NeutralTickets = 0.f;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Replicated)
 	TEnumAsByte<EGameState> GameEnum;
 	
 };
